5a72ea5f7b7ac682e8d08edca0e57715769fa43f
Castellani <bruno-castellani@hotmail.com>
2019-09-03 19:24:16 -0300

    adicionada tela de perguntas sem funcionalidade

---
+++ app/questions/index.controller.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Questions.IndexController', Controller);
+
+    function Controller($window, UserService, FlashService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.saveUser = saveUser;
+        vm.deleteUser = deleteUser;
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+    }
+
+})();
