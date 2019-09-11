445a30c3931082f9db418421ab367c739528d051
Castellani <bruno-castellani@hotmail.com>
2019-09-10 21:14:35 -0300

    tela de pergunta gravando todas alterações

--- app/questions/index.controller.js
+++ app/questions/index.controller.js
@@ -9,8 +9,12 @@
         var vm = this;
 
         vm.user = null;
-        vm.saveUser = saveUser;
-        vm.deleteUser = deleteUser;
+        vm.addQuestion = addQuestion;
+        vm.removeQuestion = removeQuestion; 
+        vm.updateQuestions = updateQuestions;
+        vm.orderUp = orderUp;
+        vm.orderDown = orderDown;
+
 
         initController();
 
@@ -20,6 +24,74 @@
                 vm.user = user;
             });
         }
-    }
 
+        function orderUp(index)
+        {
+            var x = vm.user.questions.splice(index, 1);
+            vm.user.questions.splice(index-1, 0, x[0]);
+            saveUser();
+        }
+        function orderDown(index)
+        {
+            var x = vm.user.questions.splice(index, 1);
+            vm.user.questions.splice(index+1, 0, x[0]);
+            saveUser();
+        }
+
+        function addQuestion()
+        {
+            updateQuestions(true);
+            document.getElementById("qnew").value = "";
+        }
+
+        function removeQuestion(id)
+        {
+            document.getElementById(id).value = "";
+            updateQuestions(false);
+        }
+
+        function updateQuestions(addNew)
+        {
+            var count = 1;
+            var questionsArray = [];
+            var qList = document.getElementsByClassName("questionTextBox");
+            for(var q of qList)
+            {
+                if(q.value && q.value != "")
+                {
+                    if(q.id == "qnew" && addNew)
+                    {
+                        var qObj = {
+                            id: count,
+                            value: q.value
+                        };
+                        questionsArray.push(qObj);
+                        count++;
+                    }
+                    else if(q.id != "qnew")
+                    {
+                        var qObj = {
+                            id: count,
+                            value: q.value
+                        };
+                        questionsArray.push(qObj);
+                        count++;
+                    }
+                }        
+            }
+            vm.user.questions = questionsArray;
+            saveUser();
+        }
+
+        function saveUser()
+        {
+            UserService.Update(vm.user)
+                .then(function () {
+                    //FlashService.Success('User updated');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });
+        }
+    }
 })();
